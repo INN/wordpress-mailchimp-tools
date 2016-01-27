eea41f74f2bb77843bc64c5767171813036253f4
Ryan Nagle <rmnagle@gmail.com>
2016-01-27 11:33:03 -0600

    update composer.json, fix mailchimp-tools.php

--- mailchimp-tools.php
+++ mailchimp-tools.php
@@ -28,17 +28,6 @@ define( 'MAILCHIMP_TOOLS_TEMPLATE_DIR', MAILCHIMP_TOOLS_DIR . '/templates' );
 define( 'MAILCHIMP_TOOLS_VER', '0.0.1' );
 
 /*
- * Autoload
- */
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-		error_log('Could not locate MailChimp Tools requirements.');
-	}
-	return;
-}
-require_once( __DIR__ . '/vendor/autoload.php' );
-
-/*
  * Include pluggable functions
  */
 function mailchimp_tools_include_pluggable_funcs() {
